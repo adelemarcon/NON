626a6827ef253a1ad12f6d4e53b3161594f9735a
Adele <160780354+adelemarcon@users.noreply.github.com>
2025-01-03 22:31:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ st.title("Tu ne veux vraiment pas ? 😭")
 st.link_button("Si si je suis désolé ne pleure pas", "https://adelemarcon.github.io/DM6adele.html",type="primary",use_container_width=True)
 st.link_button("Non", "https://mauvaischoix.streamlit.app/",type="primary",use_container_width=True)
 
-st.image(https://64.media.tumblr.com/b6083a3f5313a05e7d6e4316d520e83b/03e815013dd6e187-57/s1280x1920/4177688b9c4fe586fde1d00fe15c228451d5b8e9.png)
+st.image(https://github.com/adelemarcon/NON/blob/main/tortuenon.png?raw=true)
